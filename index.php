6793d947724661b079fbd4a3680483bebffd59d9
sikander27 <41859385+sikander27@users.noreply.github.com>
2018-07-29 22:57:40 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+    <title>Tilt</title>
+    <link rel="stylesheet" type="text/css" href="sk1.css">
+    </head>
+    <body>
+    <div class="row">
+        <div class="column">
+        <h1 style="align-self:flex-end"><a href="#">Home</a></h1>
+        </div>
+        <div class="column">
+        <h1 style="align-self:flex-end"><a href="#">Gallery</a></h1>
+        </div>
+        <div class="column">
+        <h1  style="align-self:flex-end"><a href="#">Events</a></h1>
+        </div>
+        <div class="column">
+            <!---<a style="align-self:flex-end" href="#"> --><h1  style="align-self:flex-end"><a href="#">About us</a></h1>
+        </div>
+        
+        
+        </div>
+    
+    
+    </body>
+</html>
